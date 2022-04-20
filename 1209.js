eaad378d16295a9fa178a02985b44fbec72db390
gyonov7 <103579725+gyonov7@users.noreply.github.com>
2022-04-20 18:13:08 +0300

    one more js exercise added

---
+++ 1209.js
@@ -0,0 +1,12 @@
+
+
+function concatenateData (){
+
+    let firstName = "Ivan"; 
+   let lastName = "Ivanov";
+   let age = 20; 
+   let town = "Plovdiv"; 
+   let summary = firstName + " " + lastName + " @ " + age + " from " + town; 
+   console.log(summary)
+}
+concatenateData()
